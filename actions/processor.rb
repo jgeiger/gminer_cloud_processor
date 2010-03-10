7771d52660a9d489280328a3517164e31c18b90e
jgeiger <jgeiger@gmail.com>
2010-03-10 08:45:57 -0600

    go back to sending a message per annotation/closure/term

--- actions/processor.rb
+++ actions/processor.rb
@@ -2,32 +2,23 @@ require File.dirname(__FILE__) + '/support/setup'
 
 class Processor < CloudCrowd::Action
 
-  attr_accessor :terms, :annotations, :closures
-
   def process
-    @terms = []
-    @annotations = []
-    @closures = []
     process_task(input)
   end
 
   def process_task(params)
-    Crowd.submit('scheduler', {'command' => 'working', 'task_id' => params['task_id'], 'time' => Time.now.to_f})
+    send_scheduler({'command' => 'working', 'task_id' => params['task_id'], 'time' => Time.now.to_f})
     # params = {'task_id' => task.id, 'geo_accession' => task.geo_accession, 'field' => task.field, 'value' => item.send(task.field), 'description' => item.descriptive_text, 'ncbo_id' => ncbo_id, 'stopwords' => stopwords}
     create_for(params['geo_accession'], params['field'], params['value'], params['description'], params['ncbo_id'], params['stopwords'], params['email'])
-    Crowd.submit('scheduler', {'command' => 'finished', 'task_id' => params['task_id'], 'time' => Time.now.to_f})
+    send_scheduler({'command' => 'finished', 'task_id' => params['task_id'], 'time' => Time.now.to_f})
     rescue NCBOException => ex
-      Crowd.submit('scheduler', {'command' => 'failed', 'task_id' => params['task_id'], 'exception' => ex.message})
+      send_scheduler({'command' => 'failed', 'task_id' => params['task_id'], 'exception' => ex.message})
   end
 
   def create_for(geo_accession, field_name, field_value, description, ncbo_id, stopwords, email)
     cleaned = field_value.gsub(/[\r\n]+/, " ")
     hash = NCBOService.result_hash(cleaned, stopwords, email, ncbo_id)
     process_ncbo_results(hash, geo_accession, field_name, description, ncbo_id)
-    send_work_units
-    @terms = []
-    @annotations = []
-    @closures = []
   end
 
   def process_ncbo_results(hash, geo_accession, field_name, description, ncbo_id)
@@ -40,8 +31,8 @@ class Processor < CloudCrowd::Action
     if hash.keys.any?
       hash.keys.each do |key|
         current_ncbo_id, term_id = key.split("|")
-        save_term('term_id' => "#{ncbo_id}|#{term_id}", 'ncbo_id' => ncbo_id, 'term_name' => hash[key][:name])
-        save_annotation('geo_accession' => geo_accession, 'field_name' => field_name, 'ncbo_id' => ncbo_id, 'ontology_term_id' => "#{ncbo_id}|#{term_id}", 'text_start' => hash[key][:from], 'text_end' => hash[key][:to], 'description' => description)
+        send_databaser('command' => 'saveterm', 'term_id' => "#{ncbo_id}|#{term_id}", 'ncbo_id' => ncbo_id, 'term_name' => hash[key][:name])
+        send_databaser('command' => 'saveannotation', 'geo_accession' => geo_accession, 'field_name' => field_name, 'ncbo_id' => ncbo_id, 'ontology_term_id' => "#{ncbo_id}|#{term_id}", 'text_start' => hash[key][:from], 'text_end' => hash[key][:to], 'description' => description)
       end
     end
   end
@@ -51,28 +42,18 @@ class Processor < CloudCrowd::Action
       hash[key].each do |closure|
         current_ncbo_id, term_id = closure[:id].split("|")
         key_current_ncbo_id, key_term_id = key.split("|")
-        save_term('term_id' => "#{ncbo_id}|#{term_id}", 'ncbo_id' => ncbo_id, 'term_name' => closure[:name])
-        save_closure('geo_accession' => geo_accession, 'field_name' => field_name, 'term_id' => "#{ncbo_id}|#{key_term_id}", 'closure_term' => "#{ncbo_id}|#{term_id}")
+        send_databaser('command' => 'saveterm', 'term_id' => "#{ncbo_id}|#{term_id}", 'ncbo_id' => ncbo_id, 'term_name' => closure[:name])
+        send_databaser('command' => 'saveclosure', 'geo_accession' => geo_accession, 'field_name' => field_name, 'term_id' => "#{ncbo_id}|#{key_term_id}", 'closure_term' => "#{ncbo_id}|#{term_id}")
       end
     end
   end
 
-  def save_term(params)
-     @terms << {'command' => 'saveterm'}.merge!(params)
+  def send_databaser(msg)
+    Crowd.submit('databaser', msg)
   end
 
-  def save_annotation(params)
-    @annotations << {'command' => 'saveannotation'}.merge!(params)
-  end
-
-  def save_closure(params)
-    @closures << {'command' => 'saveclosure'}.merge!(params)
-  end
-
-  def send_work_units
-    Crowd.submit('databaser', terms) if terms.any?
-    Crowd.submit('databaser', annotations) if annotations.any?
-    Crowd.submit('databaser', closures) if closures.any?
+  def send_scheduler(msg)
+    Crowd.submit('scheduler', msg)
   end
 
 end
